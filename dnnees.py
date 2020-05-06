9c85c7e27e34f9e8d18e289cc1bff4c839cb31fc
Nathan <51022950+BugSpray8@users.noreply.github.com>
2020-05-06 10:32:24 +0200

    Jeu du pendu en trois étapes
    Pour réaliser un bon vieux jeu du pendu en python3 ...

---
+++ dnnees.py
@@ -0,0 +1,20 @@
+"""Ce fichier définit quelques données, sous la forme de variables,
+utiles au programme pendu"""
+
+# Nombre de coups par partie
+nb_coups = 8 
+'''mettez le nombre de coups que vous voulez
+                attention à ce que n'est pas de répercutions sur le reste de votre code'''
+
+# Nom du fichier stockant les scores
+nom_fichier_scores = "scores"
+
+# Liste des mots du pendu, choisissez vos mots 
+#exemple
+liste_mots = [
+    "voiture",
+    "moto",
+    "guerre",
+    "manga",
+    "yamaha", #je vais m'arrêter là sinon je vais faire une liste de ce que j'aime ^^
+]
